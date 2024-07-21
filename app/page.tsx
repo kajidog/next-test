37ea8bd25600ad98b2b2840fa4e0b7d3b7c2107b
Kajiwara Kensei <admin@kajidog.com>
2024-07-21 19:34:01 +0900

    feat(auth): Implement next-auth for user authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>Welcome</h1>;
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Link href="/login">login</Link>
+    </div>
+  );
 }
